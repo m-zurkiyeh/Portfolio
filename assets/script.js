1599d1d8bdc7dd6632c2cbe2a35dfff870c1a587
Malik Zurkiyeh <malik.zurkiyeh@gmail.com>
2025-09-05 16:13:35 -0400

    CSS Changes

--- assets/script.js
+++ assets/script.js
@@ -3,8 +3,9 @@ let navbar = document.querySelector('.navbar');
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 let darkmode = localStorage.getItem("dark-mode");
-const themeSwitch = document.getElementById("theme-switch");
 
+const themeSwitch = document.getElementById("theme-switch");
+const submitBtn = document.getElementById('submit-button');
 const enableDarkMode = () => {
     document.body.classList.add("dark-mode");
     localStorage.setItem("dark-mode","active");
@@ -43,15 +44,6 @@ menuIcon.onclick = () => {
     navbar.classList.toggle('active');
 }
 
-function sendEmail() {
-    let fname = document.getElementById("full-name").value;
-    let email = document.getElementById("e-mail").value;
-    let number = document.getElementById("phone-number").value;
-    let subject = document.getElementById("subject").value;
-    let message = document.getElementById("message").value;
-
-    console.log("%s  %s  %d  %s  %s",fname,email,number,subject,message);
-}
 
 /**
  * A function that calls the typingAnimation() method to after a certain period of time to induce a delay 
@@ -91,3 +83,44 @@ function typingAnimation(id, text, speed = 50) {
     }
 }
 
+document.getElementById('email-form')
+ .addEventListener('submit', function(event) {
+    event.preventDefault();
+    let fullName = document.getElementById("full-name").value;
+    let emailSubject = document.getElementById("subject").value;
+    let emailAddress = document.getElementById("contact-email").value;
+    let emailMessage = document.getElementById("message").value;
+    
+
+
+   submitBtn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_nujwank';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      submitBtn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      submitBtn.value = 'Send Email';
+      console.log(JSON.stringify(err))
+      alert(JSON.stringify(err));
+    });
+});
+
+$('.project-box').click(function(){
+    var modalId = $(this).data('modal-id');
+    console.log(modalId);
+    $(`#modal-container[data-modal-id='${modalId}']`).removeAttr('class').addClass('unfold');
+    $('body').addClass('modal-active');
+    $('body').css('overflow','hidden');
+
+})
+
+  
+$(`#modal-container[data-modal-id]`).click(function(){
+    $(this).addClass('out');
+    $('body').removeClass('modal-active');
+    $('body').css('overflow','auto');
+});
